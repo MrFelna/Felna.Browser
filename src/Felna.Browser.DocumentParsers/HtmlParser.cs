53d90c251bf184089087b36667cff76a49ea09fd
MrFelna <MrFelna@users.noreply.github.com>
2023-06-01 17:26:03 +0100

    Add empty htmlparser
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers/HtmlParser.cs
@@ -0,0 +1,13 @@
+﻿using Felna.Browser.DocumentObjectModel;
+
+namespace Felna.Browser.DocumentParsers;
+
+public class HtmlParser
+{
+    public DocumentNode Parse(string html)
+    {
+        var document = new DocumentNode();
+
+        return document;
+    }
+}
