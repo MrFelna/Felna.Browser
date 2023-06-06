12b17e68dac2052c0a4bb35de58ebabdc9482323
MrFelna <MrFelna@users.noreply.github.com>
2023-06-06 12:06:15 +0100

    Update HTML tokens
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

--- src/Felna.Browser.DocumentParsers/HtmlToken.cs
+++ src/Felna.Browser.DocumentParsers/HtmlToken.cs
@@ -1,22 +1,47 @@
 ﻿namespace Felna.Browser.DocumentParsers;
 
-internal class HtmlToken
+internal abstract class HtmlToken
 {
-    internal required HtmlTokenType TokenType { get; init; }
+    internal List<HtmlTokenAttribute> TokenAttributes { get; } = new List<HtmlTokenAttribute>();
+}
+
+internal sealed class DocTypeToken : HtmlToken
+{
+    internal string? Name { get; set; }
+    
+    internal string? PublicIdentifier { get; set; }
+    
+    internal string? SystemIdentifier { get; set; }
+    
+    internal bool ForceQuirks { get; set; }
+}
+
+internal sealed class TagToken : HtmlToken
+{
+    internal required bool IsEndTag { get; init; }
+    
+    internal bool SelfClosing { get; set; }
+
+    internal string Name { get; set; } = string.Empty;
+}
+
+internal sealed class CommentToken : HtmlToken
+{
+    internal string? Data { get; set; }
+}
+
+internal sealed class CharacterToken : HtmlToken
+{
+    internal string? Data { get; set; }
+}
+
+internal sealed class EndOfFileToken : HtmlToken
+{
 }
 
 internal class HtmlTokenAttribute
 {
+    public string? Name { get; set; }
     
-}
-
-internal enum HtmlTokenType
-{
-    Unknown = 0,
-    DocType = 1,
-    StartTag = 2,
-    EndTag = 3,
-    Comment = 4,
-    Character = 5,
-    EndOfFile = 6,
+    public string? Value { get; set; }
 }
