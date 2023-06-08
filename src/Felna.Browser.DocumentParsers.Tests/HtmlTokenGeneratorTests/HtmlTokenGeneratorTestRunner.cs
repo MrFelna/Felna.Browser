5186f343735d3f2a61f9e2e96d3c964249caae20
MrFelna <MrFelna@users.noreply.github.com>
2023-06-08 11:13:47 +0100

    Add first tokenizer test
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/HtmlTokenGeneratorTestRunner.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests;
+
+public static class HtmlTokenGeneratorTestRunner
+{
+    
+    internal static void Run(string input, IReadOnlyList<HtmlToken> expectedTokens)
+    {
+        // arrange
+        var utf8Bytes = Encoding.UTF8.GetBytes(input);
+        var stream = new MemoryStream(utf8Bytes);
+        
+        
+        // act
+        var actual = HtmlTokenGenerator.TokenizeStream(stream, Encoding.UTF8.GetDecoder()).ToList();
+        
+        // assert
+        Assert.AreEqual(expectedTokens.Count, actual.Count);
+
+        for (var i = 0; i < actual.Count; i++)
+        {
+            Assert.AreEqual(expectedTokens[i], actual[i]);
+        }
+    }
+}
---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Test000001.cs
@@ -0,0 +1,21 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Test000001
+{
+    [TestMethod]
+    public void GivenValidButNoContentHtmlBasicEmptyDocumentReturned()
+    {
+        var html = @"<!DOCTYPE html>";
+        var tokens = new List<HtmlToken>
+        {
+            new DocTypeToken
+            {
+                Name = "html",
+            },
+            new EndOfFileToken(),
+        };
+        
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    } 
+}
