88454b429cacdfd2d8a2270d025d3e5f2a18bbad
MrFelna <MrFelna@users.noreply.github.com>
2023-06-13 14:08:05 +0100

    Add initial UTF8 stream consumer
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers/StreamConsumers/IStreamConsumer.cs
@@ -0,0 +1,9 @@
+﻿namespace Felna.Browser.DocumentParsers.StreamConsumers;
+
+internal interface IStreamConsumer
+{
+    (bool Success, char character) TryGetCurrentChar();
+    (bool Success, string result) LookAhead(int charCount);
+    bool EndOfStream();
+    void ConsumeChar(int charCount = 1);
+}
---
+++ src/Felna.Browser.DocumentParsers/StreamConsumers/UTF8StreamConsumer.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+
+namespace Felna.Browser.DocumentParsers.StreamConsumers;
+
+internal class UTF8StreamConsumer : IStreamConsumer
+{
+    private const char UnrepresentableChar = (char)0xfffd;
+    private readonly List<char> _charList = new List<char>();
+    private readonly Decoder _decoder = Encoding.UTF8.GetDecoder();
+    private readonly Stream _stream;
+    
+    private bool _endOfStream;
+    private int _currentCharIndex;
+
+    internal UTF8StreamConsumer(Stream stream)
+    {
+        _stream = stream;
+    }
+
+    public (bool Success, char character) TryGetCurrentChar()
+    {
+        if (!ReadUntil(_currentCharIndex))
+            return (false, UnrepresentableChar);
+            
+        return (true, _charList[_currentCharIndex]);
+    }
+
+    public (bool Success, string result) LookAhead(int charCount)
+    {
+        if (charCount < 1)
+            throw new ArgumentOutOfRangeException(nameof(charCount), "Char count must be at least 1");
+        
+        if (!ReadUntil(_currentCharIndex + charCount - 1))
+            return (false, string.Empty);
+
+        var chars = _charList.GetRange(_currentCharIndex, charCount).ToArray();
+
+        return (true, new string(chars));
+    }
+
+    public bool EndOfStream() => _endOfStream;
+
+    public void ConsumeChar(int charCount = 1)
+    {
+        if (charCount < 1)
+            throw new ArgumentOutOfRangeException(nameof(charCount), "Char count must be at least 1");
+
+        _currentCharIndex += charCount;
+    }
+
+    private bool ReadUntil(int charIndex)
+    {
+        while (_charList.Count <= charIndex)
+        {
+            var (success, character) = TryReadNextCharacter();
+            if (!success)
+                return false;
+            _charList.Add(character);
+        }
+
+        return true;
+    }
+    
+    // https://stackoverflow.com/a/11672355
+    private (bool Success, char character) TryReadNextCharacter()
+    {
+        if (_endOfStream)
+            return (false, UnrepresentableChar);
+        
+        int byteAsInt;
+        var nextChar = new char[1];
+
+        while ((byteAsInt = _stream.ReadByte()) != -1)
+        {
+            var charCount = _decoder.GetChars(new[] {(byte) byteAsInt}, 0, 1, nextChar, 0);
+            if(charCount == 0) continue;
+
+            return (true, nextChar[0]);
+        }
+
+        _endOfStream = true;
+
+        return (false, UnrepresentableChar);
+    }
+}
