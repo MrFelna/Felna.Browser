7eb83770ed5df2430739a7a35726dc4e3cd737e3
MrFelna <MrFelna@users.noreply.github.com>
2023-06-13 12:04:45 +0100

    Add value comparison to html tokens
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

--- src/Felna.Browser.DocumentParsers/HtmlToken.cs
+++ src/Felna.Browser.DocumentParsers/HtmlToken.cs
@@ -3,6 +3,26 @@
 internal abstract class HtmlToken
 {
     internal List<HtmlTokenAttribute> TokenAttributes { get; } = new List<HtmlTokenAttribute>();
+
+    // May move these methods to tests if unused in library
+    internal abstract bool AreValueEqual(HtmlToken other);
+
+    protected bool TokenAttributesEqual(HtmlToken other)
+    {
+        if (TokenAttributes.Count != other.TokenAttributes.Count)
+            return false;
+
+        for (var i = 0; i < TokenAttributes.Count; i++)
+        {
+            var x = TokenAttributes[i];
+            var y = other.TokenAttributes[i];
+
+            if (x.Name != y.Name || x.Value != y.Value)
+                return false;
+        }
+
+        return true;
+    }
 }
 
 internal sealed class DocTypeToken : HtmlToken
@@ -14,6 +34,22 @@ internal sealed class DocTypeToken : HtmlToken
     internal string? SystemIdentifier { get; set; }
     
     internal bool ForceQuirks { get; set; }
+    
+    internal override bool AreValueEqual(HtmlToken other)
+    {
+        if (other is DocTypeToken docTypeToken)
+            return AreValueEqual(docTypeToken);
+        return false;
+    }
+    
+    internal bool AreValueEqual(DocTypeToken other)
+    {
+        return Name == other.Name
+               && PublicIdentifier == other.PublicIdentifier
+               && SystemIdentifier == other.SystemIdentifier
+               && ForceQuirks == other.ForceQuirks
+               && TokenAttributesEqual(other);
+    }
 }
 
 internal sealed class TagToken : HtmlToken
@@ -23,20 +59,65 @@ internal sealed class TagToken : HtmlToken
     internal bool SelfClosing { get; set; }
 
     internal string Name { get; set; } = string.Empty;
+    
+    internal override bool AreValueEqual(HtmlToken other)
+    {
+        if (other is TagToken tagToken)
+            return AreValueEqual(tagToken);
+        return false;
+    }
+
+    internal bool AreValueEqual(TagToken other)
+    {
+        return IsEndTag == other.IsEndTag
+               && SelfClosing == other.SelfClosing
+               && Name == other.Name
+               && TokenAttributesEqual(other);
+    }
 }
 
 internal sealed class CommentToken : HtmlToken
 {
     internal string? Data { get; set; }
+    
+    internal override bool AreValueEqual(HtmlToken other)
+    {
+        if (other is CommentToken commentToken)
+            return AreValueEqual(commentToken);
+        return false;
+    }
+
+    internal bool AreValueEqual(CommentToken other)
+    {
+        return Data == other.Data
+               && TokenAttributesEqual(other);
+    }
 }
 
 internal sealed class CharacterToken : HtmlToken
 {
     internal string? Data { get; set; }
+    
+    internal override bool AreValueEqual(HtmlToken other)
+    {
+        if (other is CharacterToken characterToken)
+            return AreValueEqual(characterToken);
+        return false;
+    }
+
+    internal bool AreValueEqual(CharacterToken other)
+    {
+        return Data == other.Data
+               && TokenAttributesEqual(other);
+    }
 }
 
 internal sealed class EndOfFileToken : HtmlToken
 {
+    internal override bool AreValueEqual(HtmlToken other)
+    {
+        return other is EndOfFileToken;
+    }
 }
 
 internal class HtmlTokenAttribute
