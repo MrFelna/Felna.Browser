22d5cca261c40df979e62c5cc7ec58e9d0e3f891
MrFelna <MrFelna@users.noreply.github.com>
2023-06-28 23:01:48 +0100

    Add initial doctype public identifier tests
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Test000015.cs
@@ -0,0 +1,29 @@
+﻿using Felna.Browser.DocumentParsers.TextReferences;
+
+namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Test000015
+{
+    [TestMethod]
+    [DataRow("<!DOCTYPE html public \"id\"")]
+    [DataRow("<!DOCTYPE html public \"id\" ")]
+    [DataRow("<!DOCTYPE html public \"id\">")]
+    [DataRow("<!DOCTYPE html public \"id\" >")]
+    [DataRow("<!DOCTYPE html public 'id'")]
+    [DataRow("<!DOCTYPE html public 'id' ")]
+    [DataRow("<!DOCTYPE html public 'id'>")]
+    [DataRow("<!DOCTYPE html public 'id' >")]
+    public void GivenDoctypeWithPublicIdentifierDoctypeTokenReturnedThenEndOfFile(string html)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(html);
+        var tagIsClosed = html.Contains(CharacterReference.GreaterThanSign, StringComparison.InvariantCulture);
+        var tokens = new List<HtmlToken>
+        {
+            new DocTypeToken {Name = $"html", PublicIdentifier = "id", ForceQuirks = !tagIsClosed},
+            new EndOfFileToken(),
+        };
+        
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    } 
+}
