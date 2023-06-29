cb2877e1bcca55fcdaea15080d657a06230ee349
MrFelna <MrFelna@users.noreply.github.com>
2023-06-29 15:20:25 +0100

    Added further public doctype tests
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Test000016.cs
@@ -0,0 +1,20 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Test000016
+{
+    [TestMethod]
+    [DataRow("<!DOCTYPE html public id")]
+    [DataRow("<!DOCTYPE html public")]
+    [DataRow("<!DOCTYPE html public>")]
+    public void GivenDoctypeWithIncompletePublicIdentifierDoctypeTokenReturnedThenEndOfFile(string html)
+    {
+        var tokens = new List<HtmlToken>
+        {
+            new DocTypeToken {Name = $"html", ForceQuirks = true},
+            new EndOfFileToken(),
+        };
+        
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    } 
+}
