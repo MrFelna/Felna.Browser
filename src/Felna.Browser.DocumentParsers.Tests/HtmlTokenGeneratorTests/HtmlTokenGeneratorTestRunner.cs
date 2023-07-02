f87692903af8334c00d55b1074122d7788ba08cd
MrFelna <MrFelna@users.noreply.github.com>
2023-07-02 20:36:25 +0100

    Start converting tests to basic list
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

--- src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/HtmlTokenGeneratorTestRunner.cs
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/HtmlTokenGeneratorTestRunner.cs
@@ -1,33 +1,61 @@
-﻿using System.Text;
-using Felna.Browser.DocumentParsers.HtmlTokens;
+﻿using Newtonsoft.Json.Linq;
 
 namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests;
 
 public static class HtmlTokenGeneratorTestRunner
 {
     
-    internal static void Run(string input, IReadOnlyList<HtmlToken> expectedTokens)
+    internal static void Run(string input, IEnumerable<HtmlToken> expectedTokens)
     {
         // arrange
         var streamConsumer = new TestStreamConsumer(input);
         var tokenGenerator = new HtmlTokenGenerator(streamConsumer);
         var tokens = new List<HtmlToken>();
+        var actual = new List<HtmlToken>(expectedTokens);
+        actual.Add(new EndOfFileToken());
+        actual.Add(new EndOfFileToken());
         
         // act
-        while (tokens.Count <= expectedTokens.Count)
+        while (tokens.Count < actual.Count)
         {
             var nextToken = tokenGenerator.GetNextToken();
             tokens.Add(nextToken);
-            if (nextToken is EndOfFileToken)
-                break;
         }
         
         // assert
-        Assert.AreEqual(expectedTokens.Count, tokens.Count);
+        Assert.AreEqual(actual.Count, tokens.Count);
 
         for (var i = 0; i < tokens.Count; i++)
         {
-            Assert.IsTrue(expectedTokens[i].AreValueEqual(tokens[i]));
+            Assert.IsTrue(actual[i].AreValueEqual(tokens[i]));
         }
+    }
+
+    internal static IEnumerable<HtmlToken> ConvertJsonToTokens(string json)
+    {
+        var jArray = JArray.Parse(json);
+        var tokens = new List<HtmlToken>();
+
+        foreach (var jToken in jArray)
+        {
+            if (jToken.Type != JTokenType.Object)
+                throw new NotSupportedException();
+
+            var jObject = (JObject) jToken;
+
+            var type = jObject["type"];
+            
+            switch (type?.Value<string>())
+            {
+                case "comment":
+                    tokens.Add(new CommentToken
+                    {
+                        Data = jObject["data"]?.Value<string>()
+                    });
+                    break;
+            }
+        }
+
+        return tokens.AsReadOnly();
     }
 }
---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/MarkupDeclarationTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests;
+
+[TestClass]
+public class MarkupDeclarationTests
+{
+    [TestMethod]
+    [DataRow("<!", @"[{""type"":""comment"",""data"":""""}]")]
+    public void GivenHtmlCorrectTokensGenerated(string html, string json)
+    {
+        var tokens = HtmlTokenGeneratorTestRunner.ConvertJsonToTokens(json);
+        
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    }
+}
