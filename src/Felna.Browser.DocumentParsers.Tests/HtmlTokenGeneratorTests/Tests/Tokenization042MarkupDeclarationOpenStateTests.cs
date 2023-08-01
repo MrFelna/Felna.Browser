9bcecb817ff763adb41f678e5c9f10c32d351e9a
MrFelna <MrFelna@users.noreply.github.com>
2023-08-01 22:54:35 +0100

    Add MarkupDeclarationOpenState tests
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization042MarkupDeclarationOpenStateTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Tokenization042MarkupDeclarationOpenStateTests
+{
+    [TestMethod]
+    // TODO: Double Hyphen, CDATA
+    [DataRow("<!DOCTYPE html>", @"[{""type"":""doctype"",""name"":""html""}]")]
+    [DataRow("<!", @"[{""type"":""comment"",""data"":""""}]")]
+    [DataRow("<!D", @"[{""type"":""comment"",""data"":""D""}]")]
+    [DataRow("<!DO", @"[{""type"":""comment"",""data"":""DO""}]")]
+    [DataRow("<!DOC", @"[{""type"":""comment"",""data"":""DOC""}]")]
+    [DataRow("<!DOCT", @"[{""type"":""comment"",""data"":""DOCT""}]")]
+    [DataRow("<!DOCTY", @"[{""type"":""comment"",""data"":""DOCTY""}]")]
+    [DataRow("<!DOCTYP", @"[{""type"":""comment"",""data"":""DOCTYP""}]")]
+    [DataRow("<!DOC\u0000TYPE>", "[{\"type\":\"comment\",\"data\":\"DOC\ufffdTYPE\"}]")]
+    public void GivenHtmlCorrectTokensGenerated(string html, string json)
+    {
+        var tokens = HtmlTokenGeneratorTestRunner.ConvertJsonToTokens(json);
+        
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    }
+}
