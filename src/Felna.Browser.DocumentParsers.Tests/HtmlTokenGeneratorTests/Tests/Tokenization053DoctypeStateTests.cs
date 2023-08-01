6e2f174fdd0c7b6f9ca916c6f326c3a718fd5d3c
MrFelna <MrFelna@users.noreply.github.com>
2023-08-01 23:14:52 +0100

    Add DoctypeState tests
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization053DoctypeStateTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Tokenization053DoctypeStateTests
+{
+    [TestMethod]
+    [DataRow("<!DOCTYPE html>", @"[{""type"":""doctype"",""name"":""html""}]")]
+    [DataRow("<!DOCTYPE\thtml>", @"[{""type"":""doctype"",""name"":""html""}]")]
+    [DataRow("<!DOCTYPE\nhtml>", @"[{""type"":""doctype"",""name"":""html""}]")]
+    [DataRow("<!DOCTYPE\fhtml>", @"[{""type"":""doctype"",""name"":""html""}]")]
+    [DataRow("<!DOCTYPE \thtml>", @"[{""type"":""doctype"",""name"":""html""}]")]
+    [DataRow("<!DOCTYPE\t\nhtml>", @"[{""type"":""doctype"",""name"":""html""}]")]
+    [DataRow("<!DOCTYPE\n\fhtml>", @"[{""type"":""doctype"",""name"":""html""}]")]
+    [DataRow("<!DOCTYPE\f html>", @"[{""type"":""doctype"",""name"":""html""}]")]
+    [DataRow("<!DOCTYPE>", @"[{""type"":""doctype"",""forcequirks"":true}]")]
+    [DataRow("<!DOCTYPE", @"[{""type"":""doctype"",""forcequirks"":true}]")]
+    [DataRow("<!DOCTYPEhtml>", @"[{""type"":""doctype"",""name"":""html""}]")]
+    public void GivenHtmlCorrectTokensGenerated(string html, string json)
+    {
+        var tokens = HtmlTokenGeneratorTestRunner.ConvertJsonToTokens(json);
+        
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    }
+}
