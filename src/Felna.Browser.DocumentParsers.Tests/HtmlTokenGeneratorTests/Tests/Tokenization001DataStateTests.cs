c08593a093fc46f3013afc759e78bf972a629d95
MrFelna <MrFelna@users.noreply.github.com>
2023-09-19 11:25:50 +0100

    Migrate empty test to data state test
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization001DataStateTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Tokenization001DataStateTests
+{
+    [TestMethod]
+    // TODO: Ampersand
+    // Less-than sign
+    [DataRow("<!DOCTYPE html>", @"[{""type"":""doctype"",""name"":""html""}]")]
+    // TODO: NULL
+    // EOF
+    [DataRow("", "[]")]
+    // TODO: Anything else
+    public void GivenHtmlCorrectTokensGenerated(string html, string json)
+    {
+        var tokens = HtmlTokenGeneratorTestRunner.ConvertJsonToTokens(json);
+        
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    }
+}
