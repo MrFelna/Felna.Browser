70260979ca16a084f76bb7f8df40adffb88f88cc
MrFelna <MrFelna@users.noreply.github.com>
2024-04-23 12:18:41 +0100

    Add Commend End Bang State tests
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization052CommentEndBangStateTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Tokenization052CommentEndBangStateTests 
+{
+    [TestMethod]
+    // Hyphen-minus
+    [DataRow("<!----!-", @"[{""type"":""comment"",""data"":""--!""}]")]
+    // Greater-than sign
+    [DataRow("<!----!>", @"[{""type"":""comment"",""data"":""""}]")]
+    // EOF
+    [DataRow("<!----!", @"[{""type"":""comment"",""data"":""""}]")]
+    // Anything else
+    [DataRow("<!----!2", @"[{""type"":""comment"",""data"":""--!2""}]")]
+    public void GivenHtmlCorrectTokensGenerated(string html, string json)
+    {
+        var tokens = HtmlTokenGeneratorTestRunner.ConvertJsonToTokens(json);
+        
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    }
+}
