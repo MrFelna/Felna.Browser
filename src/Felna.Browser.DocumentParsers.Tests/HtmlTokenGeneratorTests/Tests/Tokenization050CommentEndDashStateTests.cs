d49e11fef8e5c64a226be3e706662bc9aec04039
MrFelna <MrFelna@users.noreply.github.com>
2024-04-23 12:16:05 +0100

    Add Comment End Dash State tests
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization050CommentEndDashStateTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Tokenization050CommentEndDashStateTests 
+{
+    [TestMethod]
+    // Hyphen-minus
+    [DataRow("<!----!--", @"[{""type"":""comment"",""data"":""--!""}]")]
+    [DataRow("<!--test--", @"[{""type"":""comment"",""data"":""test""}]")]
+    [DataRow("<!--test<!--", @"[{""type"":""comment"",""data"":""test<!""}]")]
+    // EOF
+    [DataRow("<!----!-", @"[{""type"":""comment"",""data"":""--!""}]")]
+    [DataRow("<!--test-", @"[{""type"":""comment"",""data"":""test""}]")]
+    [DataRow("<!--test<!-", @"[{""type"":""comment"",""data"":""test<!""}]")]
+    // Anything else
+    [DataRow("<!----!-2", @"[{""type"":""comment"",""data"":""--!-2""}]")]
+    [DataRow("<!--test-2", @"[{""type"":""comment"",""data"":""test-2""}]")]
+    [DataRow("<!--test<!-2", @"[{""type"":""comment"",""data"":""test<!-2""}]")]
+    public void GivenHtmlCorrectTokensGenerated(string html, string json)
+    {
+        var tokens = HtmlTokenGeneratorTestRunner.ConvertJsonToTokens(json);
+        
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    }
+}
