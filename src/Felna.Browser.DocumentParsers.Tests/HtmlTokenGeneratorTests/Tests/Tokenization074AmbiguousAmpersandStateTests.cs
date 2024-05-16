698827907534044d34c51e2a2065cb35354b9c03
MrFelna <MrFelna@users.noreply.github.com>
2024-05-16 22:09:33 +0100

    Add Ambiguous Ampersand State tests
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization074AmbiguousAmpersandStateTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Tokenization074AmbiguousAmpersandStateTests
+{
+    [TestMethod]
+    // TODO Consumed as part of an attribute
+    // ASCII alphanumeric
+    [DataRow("&p1", @"[{""type"":""character"",""data"":""&""},{""type"":""character"",""data"":""p""},{""type"":""character"",""data"":""1""}]")]
+    // Semi colon
+    [DataRow("&p;", @"[{""type"":""character"",""data"":""&""},{""type"":""character"",""data"":""p""},{""type"":""character"",""data"":"";""}]")]
+    // Anything else
+    [DataRow("&p&", @"[{""type"":""character"",""data"":""&""},{""type"":""character"",""data"":""p""},{""type"":""character"",""data"":""&""}]")]
+    public void GivenHtmlCorrectTokensGenerated(string html, string json)
+    {
+        var tokens = HtmlTokenGeneratorTestRunner.ConvertJsonToTokens(json);
+
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    }
+}
