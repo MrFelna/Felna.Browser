e3204ba109ee5630792a812723289e0b7402d1e5
MrFelna <MrFelna@users.noreply.github.com>
2024-05-27 23:00:29 +0100

    Move latest tests to their own file
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

--- src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization074AmbiguousAmpersandStateTests.cs
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization074AmbiguousAmpersandStateTests.cs
@@ -18,21 +18,3 @@ public class Tokenization074AmbiguousAmpersandStateTests
         HtmlTokenGeneratorTestRunner.Run(html, tokens);
     }
 }
-
-[TestClass]
-public class Tokenization075NumericCharacterReferenceStateTests
-{
-    [TestMethod]
-    // X
-    [DataRow("&#x20;", @"[{""type"":""character"",""data"":"" ""}]")]
-    // Anything else
-    [DataRow("&#", @"[{""type"":""character"",""data"":""&""},{""type"":""character"",""data"":""#""}]")]
-    [DataRow("&#32;", @"[{""type"":""character"",""data"":"" ""}]")]
-    [DataRow("&#p", @"[{""type"":""character"",""data"":""&""},{""type"":""character"",""data"":""#""},{""type"":""character"",""data"":""p""}]")]
-    public void GivenHtmlCorrectTokensGenerated(string html, string json)
-    {
-        var tokens = HtmlTokenGeneratorTestRunner.ConvertJsonToTokens(json);
-
-        HtmlTokenGeneratorTestRunner.Run(html, tokens);
-    }
-}
---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization075NumericCharacterReferenceStateTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Tokenization075NumericCharacterReferenceStateTests
+{
+    [TestMethod]
+    // X
+    [DataRow("&#x20;", @"[{""type"":""character"",""data"":"" ""}]")]
+    // Anything else
+    [DataRow("&#", @"[{""type"":""character"",""data"":""&""},{""type"":""character"",""data"":""#""}]")]
+    [DataRow("&#32;", @"[{""type"":""character"",""data"":"" ""}]")]
+    [DataRow("&#p", @"[{""type"":""character"",""data"":""&""},{""type"":""character"",""data"":""#""},{""type"":""character"",""data"":""p""}]")]
+    public void GivenHtmlCorrectTokensGenerated(string html, string json)
+    {
+        var tokens = HtmlTokenGeneratorTestRunner.ConvertJsonToTokens(json);
+
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    }
+}
