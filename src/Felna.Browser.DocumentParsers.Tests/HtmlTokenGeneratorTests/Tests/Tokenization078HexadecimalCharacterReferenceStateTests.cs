215f4e3724238cad66f4903486d89b699f40e1f1
MrFelna <MrFelna@users.noreply.github.com>
2024-06-02 16:07:17 +0100

    Add character reference state tests
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization078HexadecimalCharacterReferenceStateTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Tokenization078HexadecimalCharacterReferenceStateTests
+{
+    [TestMethod]
+    // Digit
+    [DataRow("&#x20;", @"[{""type"":""character"",""data"":"" ""}]")]
+    // Upper Hex
+    [DataRow("&#x2B;", @"[{""type"":""character"",""data"":""+""}]")]
+    [DataRow("&#xA3;", @"[{""type"":""character"",""data"":""£""}]")]
+    // Lower hex
+    [DataRow("&#x2b;", @"[{""type"":""character"",""data"":""+""}]")]
+    [DataRow("&#xa3;", @"[{""type"":""character"",""data"":""£""}]")]
+    // Semi colon
+    [DataRow("&#x20;", @"[{""type"":""character"",""data"":"" ""}]")]
+    // Anything else
+    [DataRow("&#x20", @"[{""type"":""character"",""data"":"" ""}]")]
+    [DataRow("&#x20p", @"[{""type"":""character"",""data"":"" ""},{""type"":""character"",""data"":""p""}]")]
+    public void GivenHtmlCorrectTokensGenerated(string html, string json)
+    {
+        var tokens = HtmlTokenGeneratorTestRunner.ConvertJsonToTokens(json);
+
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    }
+}
---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization079DecimalCharacterReferenceStateTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Tokenization079DecimalCharacterReferenceStateTests
+{
+    [TestMethod]
+    // Digit
+    [DataRow("&#32;", @"[{""type"":""character"",""data"":"" ""}]")]
+    // Semi colon
+    [DataRow("&#32;", @"[{""type"":""character"",""data"":"" ""}]")]
+    // Anything else
+    [DataRow("&#32", @"[{""type"":""character"",""data"":"" ""}]")]
+    [DataRow("&#32p", @"[{""type"":""character"",""data"":"" ""},{""type"":""character"",""data"":""p""}]")]
+    public void GivenHtmlCorrectTokensGenerated(string html, string json)
+    {
+        var tokens = HtmlTokenGeneratorTestRunner.ConvertJsonToTokens(json);
+
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    }
+}
