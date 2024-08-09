0af6d44fa6fca70c182c425b2920f321a668d6cb
MrFelna <MrFelna@users.noreply.github.com>
2024-08-09 08:58:16 +0100

    Added Attribute Value Unquoted State tests
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization038AttributeValueUnquotedStateTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Tokenization038AttributeValueUnquotedStateTests
+{
+    [TestMethod]
+    // Tab
+    [DataRow("<p a=b\tc=d>", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""b"",""c"":""d""}}]")]
+    // Line feed
+    [DataRow("<p a=b\nc=d>", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""b"",""c"":""d""}}]")]
+    // Form feed
+    [DataRow("<p a=b\fc=d>", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""b"",""c"":""d""}}]")]
+    // Space
+    [DataRow("<p a=b c=d>", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""b"",""c"":""d""}}]")]
+    // Ampersand
+    [DataRow("<p a=&apos; >", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""'""}}]")]
+    // Greater-than sign
+    [DataRow("<p a=b>", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""b""}}]")]
+    // NULL
+    [DataRow("<p a=\u0000>", "[{\"type\":\"tag\",\"name\":\"p\",\"attributes\":{\"a\":\"\ufffd\"}}]")]
+    // Quotation mark
+    [DataRow("<p a=b\">", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""b\""""}}]")]
+    // Apostrophe
+    [DataRow("<p a=b'>", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""b'""}}]")]
+    // Less-than sign
+    [DataRow("<p a=<>", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""<""}}]")]
+    // Equals
+    [DataRow("<p a==>", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""=""}}]")]
+    // Space
+    [DataRow("<p a=`>", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""`""}}]")]
+    // EOF
+    [DataRow("<p a=b", "[]")]
+    // Anything else
+    [DataRow("<p a=b1>", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""b1""}}]")]
+    public void GivenHtmlCorrectTokensGenerated(string html, string json)
+    {
+        var tokens = HtmlTokenGeneratorTestRunner.ConvertJsonToTokens(json);
+        
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    }
+}
