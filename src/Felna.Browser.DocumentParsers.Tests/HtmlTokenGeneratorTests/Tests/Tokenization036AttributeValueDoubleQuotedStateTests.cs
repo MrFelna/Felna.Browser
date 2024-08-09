8393c64ef85d9829cff52b3e93cd3895bab63159
MrFelna <MrFelna@users.noreply.github.com>
2024-08-09 08:41:25 +0100

    Add Attribute Value Quoted State tests
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

---
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization036AttributeValueDoubleQuotedStateTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Felna.Browser.DocumentParsers.Tests.HtmlTokenGeneratorTests.Tests;
+
+[TestClass]
+public class Tokenization036AttributeValueDoubleQuotedStateTests
+{
+    [TestMethod]
+    // Quotation mark
+    [DataRow("<p a=\"\">", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""""}}]")]
+    // Ampersand
+    [DataRow("<p a=\"&quot;\">", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""\""""}}]")]
+    // NULL
+    [DataRow("<p a=\"\u0000\">", "[{\"type\":\"tag\",\"name\":\"p\",\"attributes\":{\"a\":\"\ufffd\"}}]")]
+    // EOF
+    [DataRow("<p a=\"", "[]")]
+    // Anything else
+    [DataRow("<p a=\"b\">", @"[{""type"":""tag"",""name"":""p"",""attributes"":{""a"":""b""}}]")]
+    public void GivenHtmlCorrectTokensGenerated(string html, string json)
+    {
+        var tokens = HtmlTokenGeneratorTestRunner.ConvertJsonToTokens(json);
+        
+        HtmlTokenGeneratorTestRunner.Run(html, tokens);
+    }
+}
