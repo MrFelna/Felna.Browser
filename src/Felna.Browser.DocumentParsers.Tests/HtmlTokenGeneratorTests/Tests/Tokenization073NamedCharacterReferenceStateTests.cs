05c95ea61d45bb632fdb38a765da76f6ca624e6e
MrFelna <MrFelna@users.noreply.github.com>
2024-08-09 23:53:19 +0100

    Add test to cover historical named character references in attribute values
    Signed-off-by: MrFelna <MrFelna@users.noreply.github.com>

--- src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization073NamedCharacterReferenceStateTests.cs
+++ src/Felna.Browser.DocumentParsers.Tests/HtmlTokenGeneratorTests/Tests/Tokenization073NamedCharacterReferenceStateTests.cs
@@ -66,4 +66,43 @@ public class Tokenization073NamedCharacterReferenceStateTests
             HtmlTokenGeneratorTestRunner.Run(html, tokens);
         }
     }
+    
+    [TestMethod]
+    public void EnsureAllNamedCharactersNotEndingInSemiColonDontMatchInAttributeValue()
+    {
+        foreach (var row in NamedEntityReference.Entities)
+        {
+            var attr = row.Key;
+            if (attr.EndsWith(";", StringComparison.InvariantCulture))
+                continue;
+
+            var html = $"<p a='{attr}='>"; // realistic example: href='index.html?gt=1'
+
+            var token = new TagToken { Name = "p", IsEndTag = false };
+            token.TokenAttributes.Add(new HtmlTokenAttribute{Name = "a", Value = attr});
+            
+            HtmlTokenGeneratorTestRunner.Run(html, new [] { token });
+
+            html = $"<p a='{attr}z'>"; // realistic example: value='arts&ampcrafts'
+
+            token = new TagToken { Name = "p", IsEndTag = false };
+            token.TokenAttributes.Add(new HtmlTokenAttribute{Name = "a", Value = attr + "z"});
+            
+            HtmlTokenGeneratorTestRunner.Run(html, new [] { token });
+
+            html = $"<p a='{attr}Z'>"; // realistic example: value='arts&ampcrafts'
+
+            token = new TagToken { Name = "p", IsEndTag = false };
+            token.TokenAttributes.Add(new HtmlTokenAttribute{Name = "a", Value = attr + "Z"});
+            
+            HtmlTokenGeneratorTestRunner.Run(html, new [] { token });
+
+            html = $"<p a='{attr}1'>"; // realistic example: value='arts&ampcrafts'
+
+            token = new TagToken { Name = "p", IsEndTag = false };
+            token.TokenAttributes.Add(new HtmlTokenAttribute{Name = "a", Value = attr + "1"});
+            
+            HtmlTokenGeneratorTestRunner.Run(html, new [] { token });
+        }
+    }
 }
